63336cdf49a824be19bfadf4a7e724d01a80d928
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2023-12-16 18:48:51 +0100

    First tournament structure implemented

--- code/KartTournament.py
+++ code/KartTournament.py
@@ -8,7 +8,8 @@ class KartTournament:
     def __init__(self, playerproperties,parentTournament_A=None,parentTournament_B=None,recombinationMode=RM_PMX):
         self._playerproperties = playerproperties
         self._seatinglist=[]
-        self._seatstructure=[]
+        self._pairsize=[4,4,3] #todo determine pairsize from playerproperties
+        self._pairings=[]
         if parentTournament_A == None or parentTournament_B == None:
             self.build_gene_from_scratch()
         elif recombinationMode==RM_PMX:
@@ -28,10 +29,34 @@ class KartTournament:
             lady_luck=randrange(0,len(drawBowl))
             self._seatinglist.append(drawBowl[lady_luck])
             drawBowl.pop(lady_luck)
-    def print_seating(self):
+        self.deriveTournamentStructure()
+
+    def deriveTournamentStructure(self):
+        paired_teams=[]
+        team_seats = []
         for player_index in self._seatinglist:
-            playerproperty = self._playerproperties[player_index]
-            print(f"{playerproperty['name']} ({playerproperty['skill']})")
+            if len(team_seats)>= self._pairsize[len(self._pairings)]: # current team is complete
+                paired_teams.append(team_seats)
+                team_seats=[]
+                if len(paired_teams)>=2: # the pairing has two members
+                    self._pairings.append(paired_teams)
+                    paired_teams=[]
+            # place member in the current team
+            team_member={'player_index':player_index}
+            team_seats.append(team_member)
+        paired_teams.append(team_seats)
+        self._pairings.append(paired_teams)
+
+    def print_pairings(self):
+        for paired_teams in self._pairings:
+            print (">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            for team_index,team_seats in enumerate(paired_teams):
+                if team_index>0:
+                    print("       vs             ")
+                for team_member in team_seats:
+                    playerproperty = self._playerproperties[team_member['player_index']]
+                    print(f"{playerproperty['name']} ({playerproperty['skill']})")
+            print ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 # end of class KartTournament
 
 playerproperties = [{'name':'Mario',      'skill':1.25},{'name':'Luigi'         , 'skill':1.25}
@@ -42,9 +67,12 @@ playerproperties = [{'name':'Mario',      'skill':1.25},{'name':'Luigi'         
                     ,{'name':'Bowser Jr', 'skill':0.75},{'name':'Donkey Kong'   , 'skill':0.75}
                     ,{'name':'Diddy Kong','skill':0.75},{'name':'Baby Mario'    , 'skill':0.75}
                     ,{'name':'Baby Daisy','skill':0.75},{'name':'Baby Luigi'    , 'skill':0.75}
-]
+                    ,{'name':'Wiggler','skill': 1.25}, {'name': 'Baby Peach', 'skill': 1.00}
+                    ,{'name':'King Boo','skill': 1.25}, {'name': 'Baby Rosalina', 'skill': 1.00}
+                    , {'name': 'Lakitu', 'skill': 1.00}
+                    ]
 
 if __name__ == '__main__':
     myFirstGame=KartTournament(playerproperties)
-    myFirstGame.print_seating()
+    myFirstGame.print_pairings()
 
